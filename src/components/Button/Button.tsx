d179cc71b890253896c7faed31f4c71656574607
Sumit Kumar02 <70070228@PCHFLLAPR6409.local>
2024-05-17 01:55:01 +0530

    added storybook

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,14 @@
+import React from 'react';
 import './Button.css';
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     variant?: 'primary' | 'secondary';
     label: string;
+    size?: 'small' | 'medium' | 'large'
 }
 
-const Button: React.FC<ButtonProps> = ({ variant, label, ...props }) => {
-    const colorClass = variant === 'primary' ? 'blue' : 'green'; 
-    return <button style={{ backgroundColor: colorClass }} { ...props } >{label}</button>;
+const Button: React.FC<ButtonProps> = ({ variant, label, size='small' ,...props }) => {
+    const colorClass = variant === 'primary' ? 'pink' : 'green'; 
+    return <button className={['lus_button', `lus_button__${size}`].join(' ')} style={{ backgroundColor: colorClass }} { ...props } >{label}</button>;
 };
 
 export default Button;
