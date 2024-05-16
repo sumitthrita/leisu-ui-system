09e5e598f3f8d0dd51932b5f4d1e7d1af42d4084
Sumit Kumar02 <70070228@PCHFLLAPR6409.local>
2024-05-17 00:36:15 +0530

    added css

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,12 @@
-type ButtonProps = {
+import './Button.css';
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    variant?: 'primary' | 'secondary';
     label: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ label }) => {
-    return <div>{label}</div>;
+const Button: React.FC<ButtonProps> = ({ variant, label, ...props }) => {
+    const colorClass = variant === 'primary' ? 'blue' : 'green'; 
+    return <button style={{ backgroundColor: colorClass }} { ...props } >{label}</button>;
 };
 
 export default Button;
