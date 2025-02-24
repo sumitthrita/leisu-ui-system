6c95fc703b314bc625092ca96421a77d19016885
70070228 <sumit.kumar02@piramal.com>
2025-02-24 14:34:44 +0530

    fixed button propTypes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,2 @@
-export { default as Button } from './Button';
+export { default as Button, LusButtonState, LusButtonVariant, LusButtonSize, LusButtonWidth, LusButtonDesign } from './Button';
 export { default as Heading } from './Heading';
